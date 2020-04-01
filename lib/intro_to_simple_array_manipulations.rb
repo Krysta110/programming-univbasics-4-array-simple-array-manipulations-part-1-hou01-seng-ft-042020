0941fa4d09b3cd3a65e7d7d77c5b45cad89dbdfa
Krysta Wolkowiecki <krystawolkowiecki@outlook.com>
2020-04-01 05:59:20 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -14,3 +14,13 @@ def pop_with_args(array)
   popped_off = array.pop(2)
   return popped_off
 end
+
+def using_shift(array)
+  shift_me = array.shift
+  return shift_me
+end
+
+def shift_with_args(array)
+  shift_two = array.shift(2)
+  return shift_two
+end
